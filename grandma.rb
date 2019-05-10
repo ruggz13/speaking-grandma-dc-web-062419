03f70debafbe8d74b4660e206c94bca90cd5f415
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-10 20:25:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,5 +14,8 @@
 def speak_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif (phrase.upcase == false)
+    return "HUH?! SPEAK UP, SONNY!"
+
   end
 end
